fce9a0e6783b05873ba72538118112eec0afdd08
Margherita <145118725+Margherita97@users.noreply.github.com>
2023-12-21 13:01:45 +0100

    Add functions checkWinner & playRound

--- js/script.js
+++ js/script.js
@@ -5,4 +5,36 @@ function getComputerChoice() {
     return choice;
 }
 
-getComputerChoice();
+function checkWinner(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return "Tie";
+} 
+else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+){ 
+    return "Player";
+}
+else {
+    return "Computer";
+}
+}
+
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a tie!"
+    }
+    else if(result == "Player") {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+ 
